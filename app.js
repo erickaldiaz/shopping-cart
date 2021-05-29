1a2ae32ef67a613b3487481301405c1ae13afdf0
Ericka Diaz <erickaldiaz@gmail.com>
2021-05-29 16:39:08 -0300

    fix class Products

--- app.js
+++ app.js
@@ -14,14 +14,21 @@ const productsDOM = document.querySelector('.products-center');
 let cart = [];
 
 //getting the products
-guarda la info que trae del json en result
-En data convierte esa info en un json
+//guarda la info que trae del json en result
+//En data convierte esa info en un json
 class Products {
     async getProducts(){
         try {
             let result = await fetch ("products.json");
             let data = await result.json();
-            return data;
+            let products = data.items;
+            products = products.map(item => {
+                const { title, price } = item.fields;
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title, price, id, image };
+            });
+            return products;
         }catch(error) {
             console.log(error);
         }
@@ -41,5 +48,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const products = new Products();
 
     //get all products
-    products.getProducts().then(dat => console.log(data));
+    products.getProducts().then(data => console.log(data));
 });
